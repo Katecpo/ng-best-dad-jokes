d5b6f163c3d5eff4fc8c6f2b523cde5a45a7e9bc
Benoît Despierres <Benoit.Despierres@diiage.org>
2021-12-13 10:40:36 +0100

    feat(partie 2): Routing

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import { Post } from '../models/post.interface';
 import {PostService} from "../services/post.service";
 
@@ -13,11 +13,19 @@ export class PostComponent implements OnInit {
   post?: Post;
   @Input() isLoading = true;
 
-  constructor(private _postService: PostService, private route: ActivatedRoute) {}
+  constructor(private _postService: PostService, private route: ActivatedRoute, private _router: Router) {}
 
   async ngOnInit(): Promise<void> {
     this.posts = await this._postService.getAllPosts();
-    this.post = this.posts.find((post) => post.id === +this.route.snapshot.params['id']);
+
+    this.post = this.posts.find((post) => post.slug === this.route.snapshot.params['slug']);
+    this._router.events.subscribe(val => {
+        this.post = this.posts.find((post) => post.slug === this.route.snapshot.params['slug']);
+      }
+    );
+
     this.isLoading = false;
   }
+
+
 }
